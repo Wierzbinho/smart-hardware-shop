0912ec564cd978d87c22026faadfe4e945c78105
Wierzbinho <tom.wierzbicki@gmail.com>
2021-07-29 16:52:07 +0200

    added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {Main} from './main/Main';
 import { Sidebar } from './sidebar/Sidebar';
 
+import { Logo } from './sidebar/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <header className="header"><h1>Smart Hardware Shop</h1></header>
+      <header className="header"><Logo/></header>
       <main className="main"><Main/></main>
       <aside className="sidebar"><Sidebar/></aside>
     </div>
